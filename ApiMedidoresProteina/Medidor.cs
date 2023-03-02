6ae68ae9ba1566901f97cce40c216d5596f10ec3
Alejo <Alejo@DESKTOP-VJO9FRT>
2023-03-02 13:58:09 -0500

    Agregar archivos de proyecto.

---
+++ ApiMedidoresProteina/Medidor.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+
+
+namespace ApiMedidoresProteina
+{
+    public class Medidor
+    {
+
+        public async void Get()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://somexpro-t-default-rtdb.firebaseio.com/");
+                HttpResponseMessage response = await client.GetAsync("/protein-samples.json");
+                if (response.IsSuccessStatusCode)
+                {
+                    string data2 = await response.Content.ReadAsStringAsync();
+                    Dictionary<string, object> deserializarJson = JsonConvert.DeserializeObject<Dictionary<string, object>>(data2);
+
+
+                    dynamic jsonObj = JObject.Parse(data2);
+
+                    int count = 0;
+
+                    foreach (dynamic item in jsonObj) 
+                    {
+                        string primerValor = deserializarJson.Keys.ElementAt(count);
+                        string id = primerValor;
+                        string day = jsonObj[primerValor]["day"];
+                        string idMedidor = jsonObj[primerValor]["id"];
+                        string kind = jsonObj[primerValor]["kind"];
+                        string lat = jsonObj[primerValor]["lat"];
+                        string @long = jsonObj[primerValor]["long"];
+                        string lux = jsonObj[primerValor]["lux"];
+                        string month = jsonObj[primerValor]["month"];
+                        string planting = jsonObj[primerValor]["planting"];
+                        string protein = jsonObj[primerValor]["protein"];
+                        string time = jsonObj[primerValor]["time"];
+                        string year = jsonObj[primerValor]["year"];
+
+                        count = count + 1;
+
+                        string connectionString = "Data Source=DESKTOP-VJO9FRT;Initial Catalog=GENERAL_DB;Integrated Security=True";
+
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+
+                            string query = "SP_Medidores_Proteina '" + id + "', '" + day + "', '" + idMedidor + "', '" + kind + "', '" + lat + "', '" + @long + "', '" + lux + "', '" + month + "', '" + @planting + "', '" + protein + "', '" + time + "', '" + year + "'";
+
+                            using (SqlCommand command = new SqlCommand(query, connection))
+                            {
+                                connection.Open(); command.CommandTimeout = 4000;
+
+                                try
+                                {
+                                    command.ExecuteNonQuery();
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine(e);
+
+                                }
+
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ ApiMedidoresProteina/ModelMedidor.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiMedidoresProteina
+{
+    public class Medidores_Proteina
+    {
+        public string day { get; set; }
+        public string idMedidor { get; set; }
+        public string kind { get; set; }
+        public string lat { get; set; }
+        public string @long { get; set; }
+        public string lux { get; set; }
+        public string month { get; set; }
+        public string planting { get; set; }
+        public string protein { get; set; }
+        public string time { get; set; }
+        public string year { get; set; }
+    }
+
+    public class Root
+    {
+        [JsonProperty("id")]
+        public Medidores_Proteina id { get; set; }
+    }
+
+}
+    
+
